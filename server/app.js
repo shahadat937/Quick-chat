dc231a932c9ade699496c2c7a181df2a408eb63e
shahadat <shahadathossan937@gmail.com>
2026-02-12 15:26:50 +0600

    project publish on git

---
+++ server/app.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+const authRouter = require("./controllers/authController");
+app.use(express.json());
+app.use("/api/auth", authRouter);
+module.exports = app;
---
+++ server/controllers/authController.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+const User = require("./../models/user");
+const bcrypt = require("bcryptjs");
+
+router.post("/signup", async (req, res) => {
+  try {
+    //1. If the user already exists
+    const user = await User.findOne({ email: req.body.email });
+    //2. if user exists, send an error response
+    if (user) {
+      return res.send({
+        message: "User already exists.",
+        success: false,
+      });
+    }
+    //3. encrypt the password
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    req.body.password = hashedPassword;
+
+    //4. Create new user, save in DB
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(201).send({
+      message: "User created successfully!",
+      success: true,
+    });
+  } catch (error) {
+    res.send({
+      message: error.message,
+      success: false,
+    });
+  }
+});
+
+module.exports = router;
