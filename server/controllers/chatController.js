59cffcf4b4331928aba0e6512dc11c4033b24a5e
shahadat <shahadathossan937@gmail.com>
2026-02-14 13:08:53 +0600

    created api/chat/get-all-chats

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -18,4 +18,21 @@ router.post("/create-new-chat", authMiddleware, async (req, res) => {
   }
 });
 
+router.get("/get-all-chats", authMiddleware, async (req, res) => {
+  try {
+    const allChats = await Chat.find({ members: { $in: req.userId } });
+
+    res.status(200).send({
+      message: "Chat fetched successfully",
+      success: true,
+      data: allChats,
+    });
+  } catch (error) {
+    res.status(400).send({
+      message: error.message,
+      success: false,
+    });
+  }
+});
+
 module.exports = router;
