dd0c0f2699ab14551d609e0e9c0842cdb119f772
shahadat <shahadathossan937@gmail.com>
2026-02-14 12:37:29 +0600

    implementd api/chat/create-new-chat

--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const authRouter = require("./controllers/authController");
 const userRouter = require("./controllers/userController");
+const chatRouter = require("./controllers/chatController");
 
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
+app.use("/api/chat", chatRouter);
+
 module.exports = app;
